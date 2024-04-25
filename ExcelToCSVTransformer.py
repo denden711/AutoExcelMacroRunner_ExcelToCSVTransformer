4d7995c151ee1b21d33bf09e6630879b97e9835f
denden711 <aoikato.28@gmail.com>
2024-04-25 15:26:23 +0900

    Update ExcelToCSVTransformer.py

--- ExcelToCSVTransformer.py
+++ ExcelToCSVTransformer.py
@@ -1,10 +1,11 @@
 import os
 import sys
 import logging
-import win32com.client as win32
+import win32com.client as win32  # win32comライブラリを使用してExcelを操作します。
 
 def setup_logging():
     """ログ設定を初期化します。"""
+    # ログファイル名、ログレベル、ログのフォーマットを指定
     logging.basicConfig(filename='ExcelToCSVTransformer.log', level=logging.DEBUG,
                         format='%(asctime)s: %(levelname)s: %(message)s')
 
@@ -12,20 +13,20 @@ def run_macro(excel_file_path, macro_name):
     """指定されたExcelファイルでマクロを実行します。"""
     excel = None
     try:
-        # Excelアプリケーションを開始
+        # Excelアプリケーションを開始します。
         excel = win32.gencache.EnsureDispatch('Excel.Application')
-        excel.Visible = True  # Excelのウィンドウを表示する
+        excel.Visible = True  # Excelのウィンドウを表示
         logging.info("Excelアプリケーションが起動しました。")
 
-        # マクロが含まれるワークブックを開く
+        # マクロが含まれるワークブックを開きます。
         logging.info(f"ワークブックを開いています: {excel_file_path}")
         workbook = excel.Workbooks.Open(Filename=excel_file_path)
 
-        # マクロを実行
+        # マクロを実行します。
         logging.info(f"マクロを実行しています: {macro_name}")
         excel.Application.Run(macro_name)
 
-        # ワークブックを保存して閉じる
+        # ワークブックを保存して閉じます。
         workbook.Save()
         workbook.Close(True)
         logging.info("ワークブックが保存され、閉じられました。")
@@ -36,14 +37,14 @@ def run_macro(excel_file_path, macro_name):
 
     finally:
         if excel:
-            # Excelアプリケーションを閉じる
+            # Excelアプリケーションを閉じます。
             excel.Quit()
             logging.info("Excelアプリケーションが閉じられました。")
 
 def main():
     setup_logging()  # ログ設定を初期化
 
-    # 実行可能ファイルのディレクトリを取得
+    # 実行可能ファイルのディレクトリを取得します。
     if getattr(sys, 'frozen', False):
         dir_path = os.path.dirname(sys.executable)
     else:
